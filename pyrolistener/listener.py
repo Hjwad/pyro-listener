3e78e3c273b9cd3f11f0bba8951eabd9c3be9410
sos <158073869+Hjwad@users.noreply.github.com>
2026-03-19 01:18:34 +0300

    Update listener.py
    - fic

--- pyrolistener/listener.py
+++ pyrolistener/listener.py
@@ -8,7 +8,6 @@ from pyrogram.handlers import MessageHandler
 from .exceptions import TimeOut
 
 _cache = {}
-
 available_filters = [
     "text", "document", "animation", "sticker", "photo", "video",
     "contact", "location", "voice", "video_note", "venue", "dice",
@@ -26,8 +25,10 @@ class Listener:
 
         self.client = client
         self.show_output = show_output
-        _cache[client.name]={}
-        _cache[client.name]['list']=[]
+        
+        if client.name not in _cache:
+            _cache[client.name] = {}
+            _cache[client.name]['list'] = []
 
         self.client.add_handler(MessageHandler(self._handler), group=-99)
 
@@ -46,36 +47,22 @@ class Listener:
         parse_mode=None,
         reply_markup=None,
     ) -> pyrogram.types.Message:
-        """
-
-        Args:
-            chat_id (int): chat id
-            client (pyrogram.Client, optional): Pyrogram Client for Smart Plugins. Default to main client.
-            text (str, optional): Defaults to None.
-            from_id (Union[List[int], int], optional): peer id filters. Defaults to None.
-            filters (List[str], optional): list of Message attributes. Defaults to available_filters.
-            filters_type (int, optional): 1: The client will listen to any message contain one of filters, 2: The client will listen to message contain all of filters. Defaults to 1.
-            protect_content (bool, optional): Defaults to None.
-            reply_to_message_id (int, optional): Defaults to None.
-            disable_web_page_preview (bool, optional): Defaults to True.
-            timeout (int, optional): Time out. Defaults to None.
-            parse_mode (optional): Defaults to None.
-            reply_markup (optional): Defaults to None.
-
-        Raises:
-            TimeOut
-
-        Returns:
-            pyrogram.types.Message:
-        """
+        
         data = {
-                "from_id": from_id,
-                "filters": filters,
-                "chat_id": chat_id,
-                "filters_type": filters_type
+            "from_id": from_id,
+            "filters": filters,
+            "chat_id": chat_id,
+            "filters_type": filters_type
         }
-        if data in _cache[self.client.name]['list']: _cache[self.client.name]['list'].remove(data)
+        
+        if self.client.name not in _cache:
+            _cache[self.client.name] = {'list': []}
+
+        if data in _cache[self.client.name]['list']: 
+            _cache[self.client.name]['list'].remove(data)
+            
         _cache[self.client.name]['list'].append(data)
+        
         if text:
             m = await (client or self.client).send_message(
                 chat_id=chat_id,
@@ -88,57 +75,69 @@ class Listener:
             )
         else:
             m = None
-        _cache[self.client.name][json.dumps(data, ensure_ascii=False)]=m
+            
+        _cache[self.client.name][json.dumps(data, ensure_ascii=False)] = m
+        
         if timeout:
             stamp = (datetime.now() + timedelta(seconds=timeout))
         else:
             stamp = None
+
         def ___():
             while data in _cache[self.client.name]['list']:
                 if (timeout) and (datetime.now() > stamp):
-                    del _cache[self.client.name][json.dumps(data, ensure_ascii=False)]
+                    if json.dumps(data, ensure_ascii=False) in _cache[self.client.name]:
+                        del _cache[self.client.name][json.dumps(data, ensure_ascii=False)]
                     _cache[self.client.name]['list'].remove(data)
                     raise TimeOut("Time out error")
-                sleep(0)
-            return _cache[self.client.name][json.dumps(data, ensure_ascii=False)]
+                sleep(0.1)
+            return _cache[self.client.name].get(json.dumps(data, ensure_ascii=False))
+            
         return await (client or self.client).loop.run_in_executor(None, ___)
 
     async def listen_to(self, m: Union[pyrogram.types.CallbackQuery, pyrogram.types.Message], text : str = None,*args, **kwargs):
         if isinstance(m, pyrogram.types.CallbackQuery):
             chat_id = m.message.chat.id
             from_id = m.from_user.id
-            # reply_to_message_id = None
         elif isinstance(m, pyrogram.types.Message):
             chat_id = m.chat.id
             from_id = (m.from_user or m.sender_chat).id
-            # reply_to_message_id = m.id
         return await self.listen(chat_id=chat_id, client=m._client, from_id=from_id, text=text, *args, **kwargs)
 
     async def _handler(self, client: pyrogram.Client, message: pyrogram.types.Message):
-            sender = message.sender_chat or message.from_user
-            chat_id = message.chat.id
-            __ = []
-            for data in _cache[self.client.name]['list']:
-                if (data['chat_id'] == chat_id) and (
-                    (data["from_id"] is None) or (isinstance(data["from_id"], list) and sender.id in data["from_id"]) or (
-                        isinstance(data["from_id"], int) and data["from_id"] == sender.id
-                    )
-                ):
-                    if data["filters_type"] == 1:
-                        for _ in data["filters"]:
-                            if hasattr(message, _) and getattr(message, _):
-                                __.append(_)
-                                break
-                        if not __:
-                            return False
+        client_name = getattr(client, "name", None)
+        if not client_name or client_name not in _cache:
+            return 
 
-                    if data["filters_type"] == 2:
-                        for _ in data["filters"]:
-                            if hasattr(message, _) and getattr(message, _):
-                                __.append(_)
-                        if __ != data["filters"]:
-                            return False
-                    if self.show_output:
-                        message.output = _cache[self.client.name][json.dumps(data, ensure_ascii=False)]
-                    _cache[self.client.name][json.dumps(data, ensure_ascii=False)]=message
-                    _cache[self.client.name]['list'].remove(data)
+        sender = message.sender_chat or message.from_user
+        if not sender: return
+        
+        chat_id = message.chat.id
+        __ = []
+        
+        for data in list(_cache[client_name]['list']):
+            if (data['chat_id'] == chat_id) and (
+                (data["from_id"] is None) or 
+                (isinstance(data["from_id"], list) and sender.id in data["from_id"]) or 
+                (isinstance(data["from_id"], int) and data["from_id"] == sender.id)
+            ):
+                if data["filters_type"] == 1:
+                    for _ in data["filters"]:
+                        if hasattr(message, _) and getattr(message, _):
+                            __.append(_)
+                            break
+                    if not __: continue
+
+                if data["filters_type"] == 2:
+                    for _ in data["filters"]:
+                        if hasattr(message, _) and getattr(message, _):
+                            __.append(_)
+                    if __ != data["filters"]: continue
+
+                cache_key = json.dumps(data, ensure_ascii=False)
+                if self.show_output:
+                    message.output = _cache[client_name].get(cache_key)
+                
+                _cache[client_name][cache_key] = message
+                _cache[client_name]['list'].remove(data)
+                break
